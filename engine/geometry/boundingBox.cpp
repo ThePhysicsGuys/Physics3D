05603c828a1e6a0cf71f7bea5816644fcf6d5b3e
VonTum <lennart.vanhirtum@gmail.com>
2018-12-03 00:29:48 +0100

    Added basic BoundingBox

---
+++ engine/geometry/boundingBox.cpp
@@ -0,0 +1,36 @@
+#include "boundingBox.h"
+
+const Triangle triangles[12] = {
+	{1,0,2},{3,2,0}, // BOTTOM
+	{1,5,0},{4,0,5}, // FRONT
+	{1,2,5},{6,5,2}, // RIGHT
+	{6,2,7},{3,7,2}, // BACK
+	{3,0,7},{4,7,0}, // LEFT
+	{4,5,7},{6,7,5}, // TOP
+};
+
+bool BoundingBox::intersects(BoundingBox o) const {
+	return 
+		xmin <= o.xmax && xmax >= o.xmin &&
+		ymin <= o.ymax && ymax >= o.ymin &&
+		zmin <= o.zmax && zmax >= o.zmin;
+}
+
+bool BoundingBox::containsPoint(Vec3 point) const {
+	return 
+		point.x >= xmin && point.x <= xmax &&
+		point.y >= ymin && point.y <= ymax &&
+		point.z >= zmin && point.z <= zmax;
+}
+
+Shape BoundingBox::toShape(Vec3 vertBuf[8]) const {
+	vertBuf[0] = Vec3(xmin, ymin, zmin);
+	vertBuf[1] = Vec3(xmax, ymin, zmin);
+	vertBuf[2] = Vec3(xmax, ymax, zmin);
+	vertBuf[3] = Vec3(xmin, ymax, zmin);
+	vertBuf[4] = Vec3(xmin, ymin, zmax);
+	vertBuf[5] = Vec3(xmax, ymin, zmax);
+	vertBuf[6] = Vec3(xmax, ymax, zmax);
+	vertBuf[7] = Vec3(xmin, ymax, zmax);
+	return Shape(vertBuf, triangles, 8, 12);
+}
---
+++ engine/geometry/boundingBox.h
@@ -0,0 +1,15 @@
+#pragma once
+
+struct BoundingBox;
+
+#include "../math/vec3.h"
+#include "shape.h"
+
+struct BoundingBox {
+	double xmin, ymin, zmin;
+	double xmax, ymax, zmax;
+
+	bool intersects(BoundingBox other) const;
+	bool containsPoint(Vec3 point) const;
+	Shape toShape(Vec3 vecBuf[8]) const;
+};
