caded0d921bbc8c1979b877c13c308fde52c8486
VonTum <lennart.vanhirtum@gmail.com>
2018-12-08 01:26:47 +0100

    Added rotation movement & fixed testShape

--- engine/physical.cpp
+++ engine/physical.cpp
@@ -1,10 +1,19 @@
 #include "physical.h"
 
 
+#include "../util/log.h"
+#include "math/mathUtil.h"
 
 Physical::Physical(Part p, CFrame cframe) : part(p), cframe(cframe) {}
 
 void Physical::update(double deltaT) {
 	cframe = cframe + velocity * deltaT;
-	cframe.rotation = fromEulerAngles(0.01, 0.0, 0.0) * cframe.rotation;
+	
+	Mat3 rotation = fromRotationVec(angularVelocity * deltaT);
+
+	Log::debug("Rotation: %s", str(rotation).c_str());
+
+	angularVelocity += Vec3(0.001, 0.0, 0.0);
+
+	cframe.rotation = rotation * cframe.rotation;
 }
