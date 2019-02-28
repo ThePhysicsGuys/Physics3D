e0a70cd618049d5cc71ef8ea60b03e51dc9b4772
VonTum <lennart.vanhirtum@gmail.com>
2019-02-28 17:01:52 +0100

    Used new collision algorythm in physics (WIP)

--- engine/world.cpp
+++ engine/world.cpp
@@ -62,6 +62,27 @@ void handleTriangleIntersect(Physical& p1, Physical& p2, const Shape& transfI, c
 	}
 }
 
+void handleCollision(Physical& p1, Physical& p2, Vec3 collisionPoint, Vec3 exitVector) {
+	Vec3 v1 = p1.getVelocityOfPoint(collisionPoint - p1.getCenterOfMass());
+	Vec3 v2 = p2.getVelocityOfPoint(collisionPoint - p2.getCenterOfMass());
+	Vec3 relativeVelocity = v2 - v1;
+
+	Debug::logVec(collisionPoint, relativeVelocity, Debug::VELOCITY);
+
+	Vec3 relativeVelProjection = (relativeVelocity * exitVector) * exitVector / exitVector.lengthSquared();
+
+	Debug::logVec(collisionPoint, relativeVelProjection, Debug::IMPULSE);
+
+	// Vec3 depthForceFactor = normalVec.normalize();
+	Vec3 depthForceFactor = exitVector;
+	// Vec3 relativeSpeedFactor = (normalVec * relativeVelocity > 0) ? relativeVelProjection : Vec3();
+	Vec3 force = (depthForceFactor * 50/* + relativeSpeedFactor*10*/) * 1 / (1 / p1.mass + 1 / p2.mass);
+	p1.applyForce(collisionPoint - p1.getCenterOfMass(), force);
+	p2.applyForce(collisionPoint - p2.getCenterOfMass(), -force);
+
+	Debug::logVec(Vec3(), collisionPoint, Debug::INFO);
+}
+
 void World::tick(double deltaT) {
 
 	Vec3* vecBuf = (Vec3*) alloca(getTotalVertexCount() * sizeof(Vec3));
@@ -87,12 +108,22 @@ void World::tick(double deltaT) {
 			Physical& p2 = physicals[j];
 			Shape transfJ = transformedShapes[j];
 			
-			for(const Triangle& t1:transfI.iterTriangles()) {
+			Vec3 intersection;
+			Vec3 exitVector;
+			if(transfI.intersects(transfJ, intersection, exitVector)) {
+				Debug::logVec(p1.getCenterOfMass(), intersection - p1.getCenterOfMass(), Debug::INFO);
+				Debug::logVec(p2.getCenterOfMass(), intersection - p2.getCenterOfMass(), Debug::INFO);
+				Debug::logVec(intersection, intersection - p2.getCenterOfMass(), Debug::POSITION);
+
+				handleCollision(p1, p2, intersection, exitVector);
+			}
+
+			/*for(const Triangle& t1:transfI.iterTriangles()) {
 				for(const Triangle& t2 : transfJ.iterTriangles()) {
 					handleTriangleIntersect(p1, p2, transfI, transfJ, t1, t2);
 					handleTriangleIntersect(p2, p1, transfJ, transfI, t2, t1);
 				}
-			}
+			}*/
 		}
 	}
 
@@ -100,12 +131,12 @@ void World::tick(double deltaT) {
 		Physical& physical = physicals[i];
 		physical.update(deltaT);
 
-		Debug::logVec(physical.getCenterOfMass(), physical.angularVelocity, Debug::ANGULAR_VELOCITY);
+		/*Debug::logVec(physical.getCenterOfMass(), physical.angularVelocity, Debug::ANGULAR_VELOCITY);
 		Debug::logVec(physical.getCenterOfMass(), physical.part.cframe.localToRelative(Vec3(physical.inertia.m00 / physical.mass, 0, 0)), Debug::INFO);
 		Debug::logVec(physical.getCenterOfMass(), physical.part.cframe.localToRelative(Vec3(0, physical.inertia.m11 / physical.mass, 0)), Debug::INFO);
-		Debug::logVec(physical.getCenterOfMass(), physical.part.cframe.localToRelative(Vec3(0, 0, physical.inertia.m22 / physical.mass)), Debug::INFO);
+		Debug::logVec(physical.getCenterOfMass(), physical.part.cframe.localToRelative(Vec3(0, 0, physical.inertia.m22 / physical.mass)), Debug::INFO);*/
 
-
+		
 	}
 }
 
