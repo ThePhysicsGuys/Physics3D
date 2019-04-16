1c7a953e0b20e7a427d7b5e1bdf81f7c9da7cafe
ıllıllıOlympus112ıllıllı <matthivandersanden@gmail.com>
2019-04-16 14:59:00 +0200

    Added properties to save general and platform specific settings

---
+++ application/properties.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <string>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <algorithm>	
+
+#include "../util/log.h"
+
+class Properties {
+private:
+	std::map<std::string, std::string> properties;
+public:
+	Properties() {};
+
+	std::string get(std::string property);
+	std::map<std::string, std::string> get();
+	void add(const std::string& property, const std::string& value);
+	void remove(const std::string& property);
+
+};
+
+namespace PropertiesParser{
+	std::string ltrim(std::string string);
+	std::string rtrim(std::string string);
+	std::string trim(std::string string);
+
+	Properties read(const std::string& file);
+	void write(const std::string& file, Properties& properties);
+};
