ed0066d169aed82822fca30985af00f9dbb73a9a
moonlightware <lexsharda@gmail.com>
2020-12-20 11:36:31 -0800

    Alignment link & SSE supports

---
+++ physics/alignmentLink.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "softLink.h"
+
+
+class AlignmentLink : public SoftLink {
+public:
+	AlignmentLink(const AlignmentLink& other) = delete;
+	AlignmentLink& operator=(const AlignmentLink& other) = delete;
+	AlignmentLink(AlignmentLink&& other) = default;
+	AlignmentLink& operator=(AlignmentLink&& other) = default;
+
+	AlignmentLink(const AttachedPart& part1, const AttachedPart& part2);
+	void update() override;
+
+private:
+	Vec3 getGlobalMoment();
+	Rotation offset;
+	
+};
